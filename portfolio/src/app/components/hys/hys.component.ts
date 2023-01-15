c96b93c809ffbbfc31f2aeb1263fed26c6056b08
matiasclaro <105170674+matiasclaro@users.noreply.github.com>
2023-01-15 11:17:45 -0300

    hard and soft skill

---
+++ portfolio/src/app/components/hys/hys.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hys',
+  templateUrl: './hys.component.html',
+  styleUrls: ['./hys.component.css']
+})
+export class HysComponent {
+
+
+}
